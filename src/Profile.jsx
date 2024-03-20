c74924166cec947ff1321de35a633122ed48bc6c
svout <ivankovalchuk.service@gmail.com>
2024-03-20 23:34:16 +0000

    Summary

---
+++ src/Profile.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Profile = ({name, tag, location, image, stats}) => {
+    return (
+        <div className="container">
+        <div className="profile-container">
+          <img
+            src={image}
+            alt="User avatar"
+            className="user-avatar"
+          />
+          <p className="name">{name}</p>
+          <p className="tag">@{tag}</p>
+          <p className="location">{location}</p>
+        </div>
+  
+        <ul className="profile-list">
+          <li className="profile-item">
+            <span className="followers">Followers</span>
+            <span className="followers-stats">{stats.followers}</span>
+          </li>
+          <li className="profile-item">
+            <span className="view">Views</span>
+            <span className="view-stats">{stats.views}</span>
+          </li>
+          <li className="profile-item">
+            <span className="likes">Likes</span>
+            <span className="likes-stats">{stats.likes}</span>
+          </li>
+        </ul>
+      </div>
+    )
+}
+
+export default Profile;
